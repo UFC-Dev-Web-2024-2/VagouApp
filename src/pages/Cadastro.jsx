6b9c6b5e097107af465149a45b07e936acf1b189
David Natanael Freitas <48782126+DNatanael@users.noreply.github.com>
2025-02-28 05:29:50 +0000

    tela de cadastro front

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -12,9 +12,41 @@ const Cadastro = () => {
             className="w-full h-full object-cover"
           />
         </div>
+
+
         <div className="w-1/2 p-8 bg-white">
           <h2 className="text-2xl font-bold mb-4">Cadastro Vagou</h2>
           <p className="mb-6">Crie sua conta para começar</p>
+
+          <form>
+            <div className="mb-4">
+              <label className="block text-gray-700">Nome</label>
+              <input 
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded mt-1"
+              required></input>
+            </div>
+
+            <div className="mb-4">
+              <lavel className="block text-gray-700">E-mail</lavel>
+              <input type="email"
+              className="w-full p-2 border border-gray-300 rounded mt-1"
+              required>
+              </input>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700">Senha</label>
+              <input type="password" className="w-full p-2 border border-gray-300 rounded mt-1"
+              required/>
+            
+            </div>
+
+            <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded mb-4">
+              Criar conta
+            </button>
+
+          </form>
         </div>
       </div>
     </div>
