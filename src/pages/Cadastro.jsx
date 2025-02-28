98bfb18e0f2653a7d8eb5c083a1263e6ebb84e8e
David Natanael Freitas <48782126+DNatanael@users.noreply.github.com>
2025-02-28 05:40:13 +0000

    confirmação de senha, cidade e pais, ainda não coloquei API

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -39,7 +39,24 @@ const Cadastro = () => {
               <label className="block text-gray-700">Senha</label>
               <input type="password" className="w-full p-2 border border-gray-300 rounded mt-1"
               required/>
+            </div>
             
+            <div className="mb-4">
+              <label className="block text-gray-700">Confirmação de senha</label>
+              <input type="confirm_password" className="w-full p-2 border border-gray-300 rounded mt-1"
+              required/>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700">Pais em que mora</label>
+              <input type="text" className="w-full p-2 border border-gray-300 rounded mt-1"
+              required/>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700">Cidade em que mora</label>
+              <input type="text" className="w-full p-2 border border-gray-300 rounded mt-1"
+              required/>
             </div>
 
             <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded mb-4">
